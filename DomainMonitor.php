c8a40fb9a78b8b96709dbffd285935ec7487a290
Pavel Djundik <xPaw@users.noreply.github.com>
2019-04-01 15:50:36 +0300

    Create DomainMonitor: get whois, dns, certificates for each dns entry

---
+++ DomainMonitor.php
@@ -0,0 +1,100 @@
+<?php
+
+class DomainMonitor
+{
+	public function GetWhois( string $hostname ) : array
+	{
+		$whoisParser = new Novutec\WhoisParser\Parser();
+		$whois = $whoisParser->lookup( $hostname );
+
+		$nameservers = $whois->nameserver;
+		sort( $nameservers );
+
+		$whois =
+		[
+			'nameservers' => $nameservers,
+			'created' => strtotime( $whois->created ),
+			'expires' => strtotime( $whois->expires ),
+			'changed' => strtotime( $whois->changed ),
+			'dnssec' => $whois->dnssec,
+		];
+
+		return $whois;
+	}
+
+	public function GetAddresses( string $hostname ) : array
+	{
+		$records = dns_get_record( $hostname , DNS_A | DNS_AAAA );
+		$addresses = [];
+
+		foreach( $records as $record )
+		{
+			$address = '';
+
+			switch( $record[ 'type' ] )
+			{
+				case 'A'   : $address = $record[ 'ip' ]; break;
+				case 'AAAA': $address = '[' . $record[ 'ipv6' ] . ']'; break;
+			}
+
+			$addresses[] = $address;
+		}
+
+		sort( $addresses );
+
+		return $addresses;
+	}
+
+	public function GetCertificates( string $hostname, array $addresses ) : array
+	{
+		$certificates = [];
+
+		foreach( $addresses as $address )
+		{
+			$certificateStream = self::FetchCertificate( $address, $hostname );
+			$certificate = openssl_x509_parse( $certificateStream, false );
+
+			$certificates[ $certificate[ 'serialNumberHex' ] ] =
+			[
+				'name' => $certificate[ 'name' ],
+				'commonName' => $certificate[ 'subject' ][ 'commonName' ],
+				'subjectAltName' => $certificate[ 'extensions' ][ 'subjectAltName' ],
+				'notBefore' => $certificate[ 'validFrom_time_t' ],
+				'notAfter' => $certificate[ 'validTo_time_t' ],
+			];
+		}
+
+		return $certificates;
+	}
+
+	public function FetchCertificate( string $address, string $hostname )
+	{
+		$context = stream_context_create( [
+			'ssl' =>
+			[
+				'peer_name'         => $hostname,
+				'capture_peer_cert' => true,
+				'allow_self_signed' => true,
+				'verify_peer'       => false,
+				'verify_peer_name'  => false,
+			]
+		] );
+
+		$stream = @stream_socket_client( 'ssl://' . $address . ':443', $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $context );
+
+		if( $stream === false )
+		{
+			return null;
+		}
+
+		$streamParams = stream_context_get_params( $stream );
+		fclose( $stream );
+
+		if( empty( $streamParams[ 'options' ][ 'ssl' ][ 'peer_certificate' ] ) )
+		{
+			return null;
+		}
+
+		return $streamParams[ 'options' ][ 'ssl' ][ 'peer_certificate' ];
+	}
+}
