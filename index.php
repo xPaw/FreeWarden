f105aaffe26c196dc1ee368c1bf60e87789c22cc
Pavel Djundik <xPaw@users.noreply.github.com>
2019-06-04 10:43:55 +0300

    diff to messages

--- index.php
+++ index.php
@@ -25,6 +25,50 @@ foreach( $state as $object )
 
 $diff = CompareArrays::Diff( $state, $newState );
 $diff = CompareArrays::Flatten( $diff );
-print_r( $diff );
 
 file_put_contents( __DIR__ . '/state.json', json_encode( $newState, JSON_PRETTY_PRINT ) );
+
+unset( $state, $monitor );
+
+$time = time();
+$messages = [];
+
+foreach( $diff as $path => $change )
+{
+	$message = ucfirst( $change->Type ) . ' ' . $path . ': ';
+
+	switch( $change->Type )
+	{
+		case ComparedValue::TYPE_MODIFIED: $message .= $change->OldValue . ' to ' . $change->NewValue; break;
+		case ComparedValue::TYPE_REMOVED: $message .= $change->OldValue; break;
+		case ComparedValue::TYPE_ADDED: $message .= $change->NewValue; break;
+	}
+
+	$messages[] = $message;
+}
+
+foreach( $newState as $object )
+{
+	if( isset( $object[ 'whois' ][ 'expires' ] ) && $object[ 'whois' ][ 'expires' ] - $time < 2628000 )
+	{
+		$messages[] =
+		[
+			'domain' => $object[ 'domain' ],
+			'message' => 'Domain expires in less than 30 days',
+		];
+	}
+
+	foreach( $object[ 'certificates' ] as $certificate )
+	{
+		if( $certificate[ 'notAfter' ] - $time < 2628000 )
+		{
+			$messages[] =
+			[
+				'domain' => $object[ 'domain' ],
+				'message' => 'Certificate expires in less than 30 days',
+			];
+		}
+	}
+}
+
+print_r( $messages );
